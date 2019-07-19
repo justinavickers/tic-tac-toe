3d8aea221ed396ea77412161cbfeca1e5e464bba
Justina Vickers <justinav38@gmail.com>
2019-07-18 23:02:26 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// # Tic Tac Toe
+
+// 1. Create a grid of nine boxes
+// 1. When any box is clicked a red `X` appears
+// 1. When the next box is clicked a Blue `O` appears
+// #1 and #2 are repeated until a row of 3 of the same character is created
+// When a win happens:
+// Alert the players who won
+// Disallow any further clicks on the game board
+// Display a Play Again button that resets the game
+// For a tie:
+// Alert players that game has ended in a tie
+// Display a Play Again button that resets the game
